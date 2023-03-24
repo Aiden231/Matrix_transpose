a53f735db5214d1dc39eaf6be81430cf3e3ab793
Aiden231 <swl0511@naver.com>
2023-03-24 17:32:41 +0900

    전치 행렬 프로그램 완성본

--- Matrix_transpose/main.c
+++ Matrix_transpose/main.c
@@ -1,49 +1,56 @@
 #include <stdio.h>
+#include <Windows.h>
 #define MAX_TERMS 100
 
-// 0이 아닌 값 저장
-typedef struct {
+struct element{
 	int row;
 	int col;
 	int value;
-}element;
+};
 
-typedef struct sparseMatrix {
-	element data[MAX_TERMS];
-	int rows;
-	int cols;
-	int terms;
-}SparseMatrix;
-
-SparseMatrix matrix_transpose(SparseMatrix A)
+// 포인트를 이용한 전치행렬
+void swap(int *px, int *py)
 {
-	
-}
-
-// 행렬 출력
-void matrix_print(SparseMatrix a)
-{
-	printf("----------------------------\n");
-	for (int i = 0; i < a.terms; i++) {
-		printf("%d, %d, %d", a.data[i].row, a.data[i].col, a.data[i].value);
-	}
-	printf("----------------------------\n");
+	int tmp;
+	tmp = *px;
+	*px = *py;
+	*py = tmp;
 }
 
 int main()
 {
-	SparseMatrix m = {
-		{{0,3,7}, {1,0,9},{1,5,8},{3,0,6},{3,1,5},{4,5,1},{5,2,2}}, // data
-		6, // rows
-		6, // cols
-		7 //terms
-	};
+	int rows = 0;
+	int cols = 0;
+	int terms = 0;
 
-	SparseMatrix result;
-	result = matrix_transpose(m);
+	struct element A[MAX_TERMS];
 
-	matrix_print(m);
-	matrix_print(result);
+	printf("Enter the size of rows and columns, the number of non-zero terms(Ex.0 0 0) : ");
+	
+	// 값 받기
+	scanf_s("%d %d %d", &rows, &cols, &terms);
 
+	printf("Enter row, column, and value pairs in order(Ex.0 0 0) : \n");
+
+	if (terms > 0) {
+		for (int i = 0; i < terms; i++) {
+			scanf_s("%d %d %d", &A[i].row, &A[i].col, &A[i].value);
+
+			// 전치 시키기
+			swap(&A[i].row, &A[i].col);
+		}
+	}
+	else printf(" 모든 항이 0입니다.\n");
+
+	printf("The transposed matrix is : \n");
+	// 전치 행렬 출력
+	printf("-------------------\n");
+	if (terms > 0) {
+		for (int i = 0; i < terms; i++) 
+		printf("%d %d %d\n", A[i].row, A[i].col, A[i].value);
+	}
+	printf("-------------------\n");
+
+	system("pause");
 	return 0;
 }
